f1a96caa98b1dc0fe8fc9b00d020f049aabad6d7
samuel <samuelsyameogo@gmail.com>
2020-02-10 11:51:39 -0800

    y
    My_First_Function

---
+++ class project/JS-Basic-Javascript-1.js
@@ -0,0 +1,35 @@
+
+ alert("hello,World!");
+
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr.Seuss"); 
+
+
+ 
+ document.write('Hello, World!');
+ 
+ var X = "hello, world!"
+ document.write(X);
+ 
+ var A = "This is a string";
+ document.write(A);
+
+var B = "Concatenated" + " String"
+document.write(B);
+
+var Family = "The Arezzinis", Dad = "Jeremiah" , Mom = "Hermoins"
+    Daughter = "Penny", Son = "Zorro";
+	document.write(Dad);
+	
+	var blues = " I have the blues.";
+	var blues = blues.fontcolor("blue");
+	document.write(blues);
+
+document.write(3+3);
+
+function My_First_Funtion() {
+	var str = "This text is green!";
+	var result = str.fontcolor("green");
+	document.getElementById("Green_Text").innerHTML = result;
+};
