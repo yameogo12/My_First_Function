39d2bf08ffb1193b89df0d473243bf87460dc42a
samuel <samuelsyameogo@gmail.com>
2020-02-10 13:30:36 -0800

    y
     c

--- class project/File_Name.js
+++
@@ -1,7 +0,0 @@
-alert("hello,World!");
-
-document.write("\"Be who you are and say what you feel,"
-+ "because those who mind don\'t matter and those who matter don\'t mind.\""
-+ "-Dr.Seuss"); 
-
-document.write(3+3);
---
+++ class project/JS-Basic-Javascript-2.js
@@ -0,0 +1,18 @@
+function myFunction() {
+	 var X =document.getElementById("Image");
+	 X.style.display = "none";
+ };
+
+	
+//	var voire= "VOIRE.";
+//	var voire = voire.fontcolor("red");
+//	document.write(voire);
+
+
+function monFunction(){
+	var sentence = " I am learning";
+	sentence += " a lot from this book!";
+	document.getElementById("Concatenate").innerHTML = sentence;
+};
+	
+	
